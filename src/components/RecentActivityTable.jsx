79fed933518cc5208b02e18f12c8d45f3edd8101
Dhananjay <dhananjaykhodaskar27@gmail.com>
2024-10-06 13:25:11 +0530

    added pagination to the table

--- src/components/RecentActivityTable.jsx
+++ src/components/RecentActivityTable.jsx
@@ -8,53 +8,94 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { formatTimestamp, getProblemNameFromSlug } from "@/utils/getSubmissionData";
+import {
+  formatTimestamp,
+  getProblemNameFromSlug,
+} from "@/utils/getSubmissionData";
 import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 export function RecentActivityTable({ recentTableData }) {
-  return (
-    <Table>
-      <TableCaption>A list of recent activity in your group.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Username</TableHead>
-          <TableHead>Title Slug</TableHead>
-          <TableHead>Time</TableHead>
-          <TableHead className="text-right">Language</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {recentTableData?.map((recentData) => (
-          <TableRow key={uuidv4()}>
-            <TableCell>{recentData.username}</TableCell>
-            <TableCell className="font-medium">
-              <a
-                target="_blank"
-                href={`https://leetcode.com/problems/${recentData.titleSlug}/description/`}
-              >
-                {getProblemNameFromSlug(recentData.titleSlug)}
-              </a>
-            </TableCell>
-            <TableCell>{formatTimestamp(recentData.timestamp)}</TableCell>
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10; 
 
-            <TableCell className="text-right">
-              {recentData?.lang
-                ? recentData.lang.charAt(0).toUpperCase() +
-                  recentData.lang.slice(1)
-                : "N/A"}
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+
+  const currentItems = recentTableData?.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(recentTableData?.length / itemsPerPage);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of recent activity in your group.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Username</TableHead>
+            <TableHead>Title Slug</TableHead>
+            <TableHead>Time</TableHead>
+            <TableHead className="text-right">Language</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {currentItems?.map((recentData) => (
+            <TableRow key={uuidv4()}>
+              <TableCell>{recentData.username}</TableCell>
+              <TableCell className="font-medium">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer" 
+                  href={`https://leetcode.com/problems/${recentData.titleSlug}/description/`}
+                >
+                  {getProblemNameFromSlug(recentData.titleSlug)}
+                </a>
+              </TableCell>
+              <TableCell>{formatTimestamp(recentData.timestamp)}</TableCell>
+              <TableCell className="text-right">
+                {recentData?.lang
+                  ? recentData.lang.charAt(0).toUpperCase() +
+                    recentData.lang.slice(1)
+                  : "N/A"}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={4} className="flex justify-end gap-2">
+              <Button onClick={handlePreviousPage} disabled={currentPage === 1}>
+                Previous
+              </Button>
+              <Button
+                onClick={handleNextPage}
+                disabled={currentPage === totalPages}
+              >
+                Next
+              </Button>
             </TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow></TableRow>
-      </TableFooter>
-    </Table>
+        </TableFooter>
+      </Table>
+    </div>
   );
 }
-// title: submission.title,
-// titleSlug: submission.titleSlug,
-// timestamp: submission.timestamp,
-// statusDisplay: submission.statusDisplay,
-// lang: submission.lang,
-// username: user.username,
