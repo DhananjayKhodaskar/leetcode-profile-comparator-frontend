0dee8cadf4ca12132e3aaebd0a63992e256af1f3
Dhananjay <dhananjaykhodaskar27@gmail.com>
2024-10-14 15:10:16 +0530

    basic redux setup done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,32 @@
+import { decrement, increment } from "@/slices/authSlice";
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Login = () => {
-  return <div>Login</div>;
+  const count = useSelector((state) => state.auth.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      {" "}
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
