3b9f8385d932772ba45582e2582e8d91fcc11ad5
Dhananjay <dhananjaykhodaskar27@gmail.com>
2024-10-16 00:09:24 +0530

    added image

--- src/components/AuthSideImage.jsx
+++ src/components/AuthSideImage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import authImg from "../assets/E-LeetSquad.png";
 
 const AuthSideImage = () => {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen overflow-hidden"> {/* Prevents scrolling */}
       <img
-        src="https://images.unsplash.com/photo-1579546929662-711aa81148cf?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src={authImg}
         alt="auth-side-image"
-        className="hidden md:block md:w-1/2"
+        className="hidden md:block md:w-1/2 w-full h-screen object-cover" // Sets height to the full screen height
       />
       <Outlet />
     </div>
