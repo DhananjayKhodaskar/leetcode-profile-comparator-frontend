bdce919d718993e13da1087b82f97481803c8804
Dhananjay <dhananjaykhodaskar27@gmail.com>
2024-10-16 10:30:17 +0530

    added layout and navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Sidebar } from "./Sidebar";
 import { Outlet } from "react-router-dom";
+import { Navbar } from "./Navbar";
 
 const Layout = () => {
   return (
-    <div className="flex">
-      <Sidebar className="w-64 bg-slate-500 min-h-screen" />
-      <div className="flex-1 bg-gray-100 p-4">
-        <Outlet />
+    <div className="flex min-h-screen"> {/* Ensure full height */}
+      <Sidebar className="w-64 bg-slate-500" />
+      <div className="flex flex-col flex-1"> {/* Wrap Navbar and Outlet in a flex column */}
+        <Navbar />
+        <div className="flex-1 bg-gray-100 p-4"> {/* Outlet Container */}
+          <Outlet />
+        </div>
       </div>
     </div>
   );
